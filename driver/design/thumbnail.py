89a901013d9434c3f1721b3045f2ca8b672bfb2c
Xjzutzh <98710695+Xjzutzh@users.noreply.github.com>
2022-02-05 17:53:41 +0530

    Update thumbnail.py

--- driver/design/thumbnail.py
+++ driver/design/thumbnail.py
@@ -31,18 +31,8 @@ async def thumb(thumbnail, title, userid, ctitle):
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("driver/source/regular.ttf", 50)
     font2 = ImageFont.truetype("driver/source/medium.ttf", 72)
-    draw.text(
-        (25, 615),
-        f"{title[:20]}...",
-        fill="black",
-        font=font2,
-    )
-    draw.text(
-        (27, 543),
-        f"Playing on {ctitle[:12]}",
-        fill="black",
-        font=font,
-    )
+    draw.text((27, 535), f"Powered By: PratheekBots...", fill="black", font=font)
+    draw.text((25, 610), f"{title[:18]}...", fill= "black", font=font2)
     img.save(f"search/final{userid}.png")
     os.remove(f"search/temp{userid}.png")
     os.remove(f"search/thumb{userid}.png")
